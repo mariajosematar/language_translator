d93cdbd72eddf555cab70b49b45460dd7621cd1c
Maria Jose Matar <matarmariajose@gmail.com>
2024-01-12 15:44:13 +0100

    add result

---
+++ ui/src/TranslationResult.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TranslationResult({ translation }) {
+  return (
+    <div>
+      <h2>Translation Result</h2>
+      <p>{translation}</p>
+    </div>
+  );
+}
+
+export default TranslationResult;
