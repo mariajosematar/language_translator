4ab563ea4617ca47b7df7d02bb0e461e1bc40ecf
Maria Jose Matar <matarmariajose@gmail.com>
2024-01-12 15:03:15 +0100

    change translator

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from translation.translator import TranslationService
 from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
+translator = TranslationService()
 
 class TranslationRequest(BaseModel):
     text: str
@@ -11,27 +12,18 @@ class TranslationRequest(BaseModel):
 class JeringozaTranslationRequest(BaseModel):
     text: str
 
-def translate_text(text: str, target_language: str) -> str:
-    # lógica real para traducir el texto al idioma especificado
-    translated_text = f"Translation of '{text}' to {target_language} goes here"
-    return translated_text
-
-def translate_to_jeringoza(text: str) -> str:
-    # lógica real para traducir el texto a Jeringoza
-    translated_text = f"Jeringoza translation of '{text}' goes here"
-    return translated_text
-
 @app.post("/translate", response_model=dict)
 def translate(request: TranslationRequest):
-    supported_languages = ["Spanish", "German", "French", "Italian", "Danish"]
-    
-    if request.target_language.capitalize() not in supported_languages:
-        raise HTTPException(status_code=400, detail="Unsupported language")
-
-    translated_text = translate_text(request.text, request.target_language.capitalize())
-    return {"translation": translated_text, "target_language": request.target_language}
+    try:
+        translated_text = translator.translate_text(request.text, request.target_language)
+        return {"translation": translated_text, "target_language": request.target_language}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
 
 @app.post("/translate/jeringoza", response_model=dict)
 def translate_jeringoza(request: JeringozaTranslationRequest):
-    translated_text = translate_to_jeringoza(request.text)
-    return {"translation": translated_text}
+    try:
+        translated_text = translator.translate_to_jeringoza(request.text)
+        return {"translation": translated_text}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
